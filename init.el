98638acd5e5b2bc5b7c8585c6a8fa86f21ca4c5a
itsarune <arun.sfc@outlook.com>
2020-11-29 02:30:46 -0500

    added basic code completion

--- init.el
+++ init.el
@@ -1,1 +1,146 @@
-/Users/arun/.emacs.d/init.el
+(defun indent-region-custom(numSpaces)
+    (progn 
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
+
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards            
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun untab-region (N)
+    (interactive "p")
+    (indent-region-custom -4)
+)
+
+(defun tab-region (N)
+    (interactive "p")
+   (if (active-minibuffer-window)
+        (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
+    ; else
+    (if (string= (buffer-name) "*shell*")
+        (comint-dynamic-complete) ; in a shell, use tab completion
+    ; else
+    (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
+        (indent-region-custom 4) ; region was selected, call indent-region
+        (insert "    ") ; else insert four spaces as expected
+    )))
+)
+
+(global-set-key (kbd "<backtab>") 'untab-region)
+(global-set-key (kbd "<tab>") 'tab-region)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dumb-jump exec-path-from-shell yasnippet-snippets irony-eldoc irony bazel-mode yasnippet request evil el-get)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'package)
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(require 'use-package)
+(use-package yasnippet
+	     :ensure t
+	     :config
+	     (use-package yasnippet-snippets
+			  :ensure t)
+	     (yas-reload-all))
+(yas-global-mode 1)
+(define-key yas-minor-mode-map (kbd "C-c y") 'yas-expand)
+
+;; Initializes the package infrastructure
+(package-initialize)
+ 
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Installs packages
+;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    material-theme                  ;; Theme
+    elpy
+    flycheck
+    ein
+    magit
+    py-autopep8
+    )
+)
+
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+
+;; ===================================
+;; Basic Customization
+;; ===================================
+
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'material t)            ;; Load material theme
+(global-linum-mode t)               ;; Enable line numbers globally
+
+;; ====================================
+;; Development Setup
+;; ====================================
+;; Enable elpy
+(elpy-enable)
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable autopep8
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;; Use IPython for REPL
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+
+(global-ede-mode 1)
+(require 'semantic/sb)
+(semantic-mode 1)
+
+(defun cedet-hooks ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle))
+(add-hook 'c-mode-common-hook 'cedet-hooks)
+
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+;; User-Defined init.el ends here
